d54a07b4549ad221b48d027543950fa2903eb5e1
Ajaysajwan9 <127719000+Ajaysajwan9@users.noreply.github.com>
2026-04-08 21:02:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // your code here
+document.getElementById("button").addEventListener("click", function () {
+            let name = document.getElementById("name").value;
+            let year = document.getElementById("year").value;
+
+            let baseURL = "https://localhost:8080/";
+            let queryParams = [];
+            if (name) {
+                queryParams.push("name=" + encodeURIComponent(name));
+            }
+            if (year) {
+                queryParams.push("year=" + encodeURIComponent(year));
+            }
+            let finalURL = baseURL;
+            if (queryParams.length > 0) {
+                finalURL += "?" + queryParams.join("&");
+            }
+           document.getElementById("url").innerText = finalURL;
+        });
